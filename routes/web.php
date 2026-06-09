595150dbab26c79fe5505096af66a3c7b2a00ff7
Дар'я <daria.zhuravska@oa.edu.ua>
2026-06-09 12:42:33 +0000

    Lab 13: Add Queue Chain for Catalog Generation

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::group(['prefix' => 'digging_deeper'], function () {
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
         ->name('digging_deeper.collections');
 });
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [\App\Http\Controllers\DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
+
+    Route::get('process-video', [\App\Http\Controllers\DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [\App\Http\Controllers\DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
+});
